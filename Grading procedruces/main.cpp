c91453f61dbe6f24efce800301630e5d4371dc4f
20161104591 <xyf@1.local>
2018-06-25 11:18:44 +0800

    。。。
    。。。。

--- Grading procedruces/main.cpp
+++ Grading procedruces/main.cpp
@@ -8,6 +8,25 @@
 
 #include <iostream>
 
+struct actor
+{
+    int num;
+    char name[10];
+    char sex[10];
+    char class_name[10];
+    char program_name[10];
+    char program_type[10];
+    int phone_number[10];
+};
+
+struct judge
+{
+    char name[10];
+    char sex[10];
+    int num[10];
+};
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
