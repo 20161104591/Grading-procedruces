01ae49c5bd6f70d3eb394c72d3eff912d7e42123
20161104591 <xyf@1.local>
2018-06-25 10:49:28 +0800

    Initial Commit

---
+++ Grading procedruces/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Grading procedruces
+//
+//  Created by XYF on 18/6/25.
+//  Copyright © 2018年 XYF. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
